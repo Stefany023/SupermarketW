91791d75c3ca7b435ec52227f0c0c0e01b74b372
Stefany023 <stefanyb.lopera@gmail.com>
2022-02-20 13:03:46 -0500

    advances: class creation

---
+++ SupermarketW/SupermarketW/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace SupermarketW
+{
+    public abstract class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public float Tax { get; set; }
+        public override string ToString()
+        {
+            return $"{Id}-{Description}, " +
+                $"Precio: {Price},IVA: {Tax}";
+        }
+        public abstract decimal ValueToPay();
+    }
+}
---
+++ SupermarketW/SupermarketW/VariablePriceProduct.cs
@@ -0,0 +1,18 @@
+﻿namespace SupermarketW
+{
+    public class VariablePriceProduct : Product
+
+    {
+        public float Measurement { get; set; }
+        public int Quantity { get; set; }
+        public override decimal ValueToPay()
+        {
+            return Price;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tValueToPay: {ValueToPay()}";
+        }
+    }
+}
